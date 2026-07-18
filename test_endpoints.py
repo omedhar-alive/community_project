ae029f73a9515bb9b9a1b61e5c6d289f01ced28e
Omar Medhat <omedhar@gmail.com>
2026-07-18 16:13:15 +0300

    testing login and user creation

--- test_endpoints.py
+++ test_endpoints.py
@@ -35,6 +35,23 @@ def fresh_db():
 
 
 
-def test_create_owner(fresh_db):
-    response = client.post("/owners",json={"name":"test_check","phone_number":"1414","email":"testingthetest@example.com","password":"pwpwpw"})
-    assert response.status_code == 200
+def test_owner_create_user(fresh_db):
+    created_owner = client.post("/owners",json={"name":"test_check","phone_number":"1414","email":"testingthetest@example.com","password":"pwpwpw"})
+    
+    logged_owner = client.post("/owners/login",data={"username":"testingthetest@example.com","password":"pwpwpw"})
+    token = logged_owner.json()["access_token"]
+    owner_create_user = client.post("/user",headers={"Authorization" : f"bearer {token}"},json={"name":"omartest","phone_number":"9999","email":"pytest@test.com","password":"pytest","role":"pytest","owner_id":4})
+    owner = client.get("/owners",headers={"Authorization" : f"bearer {token}"})
+    owner_id = owner.json()["id"]
+    connection = sqlite3.connect("test.db",check_same_thread=False)
+    connection.execute("PRAGMA foreign_keys= ON")
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute("SELECT owners.email AS owner_email,users.email AS user_email,users.owner_id FROM owners JOIN users ON owners.id = users.owner_id WHERE owners.id = ?",(owner_id,))
+    result = cursor.fetchall()
+    created_user = next((u for u in result if u["user_email"]=="pytest@test.com"))
+    user_owner_id = created_user["owner_id"]
+
+    assert user_owner_id == owner_id 
+
+
